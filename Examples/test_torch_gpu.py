df016d6df700340ac9e31887a1ab12eb9c7f34aa
Manuel Marcelino Titos Luzon <mmtitos@ugr.es>
2025-10-24 11:06:40 +0200

    Update test_torch_gpu.py

--- Examples/test_torch_gpu.py
+++ Examples/test_torch_gpu.py
@@ -1,1 +1,9 @@
+import torch
 
+print("PyTorch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    print("Running on CPU.")
