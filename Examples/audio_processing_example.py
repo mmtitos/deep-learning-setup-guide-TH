0e5551068de6d4574731ee232617f4a9683ca23e
Manuel Marcelino Titos Luzon <mmtitos@ugr.es>
2025-10-24 11:07:24 +0200

    Update audio_processing_example.py

--- Examples/audio_processing_example.py
+++ Examples/audio_processing_example.py
@@ -1,1 +1,13 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
 
+# Load example audio
+y, sr = librosa.load(librosa.example('trumpet'), duration=3)
+
+# Display waveform
+librosa.display.waveshow(y, sr=sr)
+plt.title("Example waveform from Librosa")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.show()
